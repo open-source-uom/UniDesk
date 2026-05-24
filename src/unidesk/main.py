f420f755974b5686a51ca0c0bba2cabe60306b32
Apostolos Chalis <tolishalis@gmail.com>
2026-05-24 19:32:14 +0300

    Corrected paths

--- src/unidesk/main.py
+++ src/unidesk/main.py
@@ -4,18 +4,22 @@ from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication
 from .home import UniOSWelcome
 
+
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("UniDesk")
 
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
     icon_path = os.path.abspath(
-        os.path.join(src_dir, "..", "resources", "unios.png")
+        os.path.join(current_dir, "..", "..", "resources", "unios.png")
     )
     app.setWindowIcon(QIcon(icon_path))
 
     window = UniOSWelcome()
     window.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
