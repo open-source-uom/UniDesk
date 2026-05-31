b6e38ec0173c891973f24137b691e11dac5c6300
Emanuela <emma.gkt@gmail.com>
2026-05-31 15:14:26 +0300

    added the form for departments

--- src/unidesk/links.py
+++ src/unidesk/links.py
@@ -5,15 +5,19 @@ LINKS = [
         "label": "Discord Channel",
         "url": "https://discord.gg/QA9AxTdppX",
     },
-     {
+    {
+        "label": "Department's software list",
+        "url": "https://docs.google.com/forms/d/e/1FAIpQLSfX27PSSPFWNH55Uq4GX2pxhh2egHBLEAk_rCPdye5924wz6Q/viewform",
+    },
+    {
         "label": "Github",
         "url": "https://github.com/open-source-uom",
     },
-      {
+    {
         "label": "UniMate",
         "url": "https://open-source-uom.github.io/UniMate/",
     },
-       {
+    {
         "label": "Uni OS Official Webpage",
         "url": "https://open-source-uom.github.io/UniOS-landing-page/",
     },
@@ -21,7 +25,7 @@ LINKS = [
         "label": "UniOS Docs",
         "url": "https://open-source-uom.github.io/UniOS-docs/",
     },
-        {
+    {
         "label": "Open Source UoM",
         "url": "http://opensource.uom.gr/",
     },
